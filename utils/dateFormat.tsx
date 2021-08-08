f9108432060add3fedc0596523e8f613c14400c4
Bagus TRIANURDIN <btstargroup@gmail.com>
2021-08-08 16:20:48 +0700

    fix(show posted date)

--- utils/dateFormat.tsx
+++ utils/dateFormat.tsx
@@ -2,5 +2,5 @@ export default function dateFormat(date: string): string {
   const DATE = new Date(date);
   const MONTH = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Dec'];
 
-  return `${MONTH[DATE.getMonth() + 1]} ${DATE.getDay()}, ${DATE.getFullYear()}`;
+  return `${MONTH[DATE.getMonth()]} ${DATE.getDate()}, ${DATE.getFullYear()}`;
 }
