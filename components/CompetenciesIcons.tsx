b315b017b7db380789034601346fbf97c32522a9
Bagus TRIANURDIN <btstargroup@gmail.com>
2021-08-23 21:07:30 +0700

    fix: change php icon color

--- components/CompetenciesIcons.tsx
+++ components/CompetenciesIcons.tsx
@@ -12,7 +12,7 @@ export default function CompetenciesIcons(): JSX.Element {
       <FaReact className={iconClass.concat('text-blue-400')} />
       <SiJavascript className={iconClass.concat('text-yellow-300')} />
       <SiTypescript className={iconClass.concat('text-blue-600')} />
-      <SiPhp className={iconClass.concat('text-blue-600')} />
+      <SiPhp className={iconClass.concat('text-indigo-500')} />
       <SiNodeDotJs className={iconClass.concat('text-green-600')} />
       <SiNextDotJs className={iconClass.concat('text-gray-900')} />
       <SiMysql className={iconClass.concat('text-yellow-500')} />
